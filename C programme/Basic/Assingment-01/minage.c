90a127770111f8604a5b65e664cec30ba02b5228
jmshahriar2-cell <jmshahriar2@gmail.com>
2026-09-13 19:56:42 +0200

    Add files via upload

---
+++ C programme/Basic/Assingment-01/minage.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Declare two int variables named age and minAge, and assign them values
+    int age = 20; 
+    int minAge = 18; 
+
+    // Use printf to print the result of this comparison: age >= minAge
+    // This will print 1 if true, or 0 if false
+    printf("%d\n", age >= minAge);
+
+    return 0;
+}
+
