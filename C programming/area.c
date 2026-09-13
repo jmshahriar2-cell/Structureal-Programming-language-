d2fe10e03de894ed482bccc0a28e226cd804e23c
jmshahriar2-cell <jmshahriar2@gmail.com>
2026-09-13 19:10:52 +0200

    Add files via upload

---
+++ C programming/area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the length of the square: ");
+    scanf("%d", &a);
+
+    printf("Enter the width of the square: ");
+    scanf("%d", &b);
+
+    printf("The area of the square is: %d", a * b);
+
+
+return 0;    
+}
