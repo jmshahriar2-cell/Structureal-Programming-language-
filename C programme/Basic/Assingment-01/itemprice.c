d7d0d30deab35696d814d3a8936e0e8f5109e515
jmshahriar2-cell <jmshahriar2@gmail.com>
2026-09-14 04:15:00 +0200

    Add files via upload

---
+++ C programme/Basic/Assingment-01/itemprice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    //Declare two int variables named itemPrice and shippingCost, and assign them values
+    int itemPrice = 25; 
+    int shippingCost = 5;
+
+    //Create an int variable named sum
+    int sum;
+
+    //Calculate the total cost by adding itemPrice and shippingCost (store the result in sum)
+    sum = itemPrice + shippingCost;
+
+    //Print the total cost using printf
+    printf("Total cost: %d\n", sum);
+
+    return 0;
+}
+
